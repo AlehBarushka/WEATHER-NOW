3861b57dc9630c84e9f1c47b630dd6b8886ed21c
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-17 14:22:21 +0300

    add new layout

--- src/App.js
+++ src/App.js
@@ -1,106 +1,36 @@
-import React, { Component } from 'react';
-import { DateTime } from 'luxon';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { API_KEY } from './api/constants';
 
-import './App.css';
+const App = () => {
+	// const url = API_KEY;
 
-const API_URL = 'https://api.openweathermap.org/data/2.5/';
-const API_KEY = '75787c308f283527b34dff6f9ed6f9b8';
-
-export default class App extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			searchQuery: '',
-			weather: null,
-			isLoading: false,
-		};
-	}
-
-	handleSearchChange = (e) => {
-		this.setState({
-			searchQuery: e.target.value,
-		});
-	};
-
-	sendRequest = () => {
-		const { searchQuery } = this.state;
-
-		fetch(`${API_URL}weather?q=${searchQuery}&appid=${API_KEY}&units=metric`)
-			.then((response) => response.json())
-			.then((response) =>
-				this.setState({
-					weather: response,
-					isLoading: false,
-				})
-			);
-	};
-
-	handleSearchSubmit = (e) => {
-		if (e.key !== 'Enter') {
-			return;
-		}
-
-		this.setState(
-			{
-				isLoading: true,
-			},
-			this.sendRequest
-		);
-	};
-
-	componentDidMount() {}
-
-	render() {
-		const { searchQuery, weather, isLoading } = this.state;
-
-		return (
-			<div
-				className={
-					weather && weather.main && weather.main.temp < 0
-						? 'container cold'
-						: 'container'
-				}
-			>
-				<input
-					type='text'
-					placeholder='Search...'
-					className={
-						weather && weather.cod === '404'
-							? 'search-input error'
-							: 'search-input'
-					}
-					value={searchQuery}
-					onChange={this.handleSearchChange}
-					onKeyDown={this.handleSearchSubmit}
-				/>
-
-				{isLoading ? <div className='loader'></div> : null}
-
-				{weather && weather.cod === 200 ? (
-					<div>
-						<div className='location-wrapper'>
-							<div className='location'>
-								{weather.name}, {weather.sys.country}
-							</div>
-
-							<div className='date'>
-								{DateTime.fromSeconds(weather.dt).toLocaleString(
-									DateTime.DATE_HUGE
-								)}
-							</div>
-						</div>
-						<div className='weather-wrapper'>
-							<div className='temp'>{Math.round(weather.main.temp)}°C</div>
-							<div className='weather'>{weather.weather[0].main}</div>
-						</div>
+	return (
+		<div className='app'>
+			<div className='container'>
+				<div className='top'>
+					<div className='location'>
+						<p>Hrodna</p>
 					</div>
-				) : null}
-
-				{weather && weather.cod === '404' ? (
-					<div className='error-message'>Неправильный ввод</div>
-				) : null}
+					<div className='temp'>
+						<h1>5C</h1>
+					</div>
+					<div className='descriptiom'>
+						<p>Clouds</p>
+					</div>
+				</div>
+				<div className='bottom'>
+					<div className='feels'>
+						<p>4C</p>
+					</div>
+					<div className='humidity'>
+						<p>20%</p>
+					</div>
+					<div className='wind'>12 км/ч</div>
+				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+export default App;
