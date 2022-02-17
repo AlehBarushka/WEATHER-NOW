9f1222499d472224b1e9a0eae58fbd462fe5dd74
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-17 16:12:15 +0300

    add current weathers fields to jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { data } from 'browserslist';
 import React, { useState } from 'react';
 import { getWeather } from './api/openWeather';
 
 const App = () => {
 	const [weatherData, setWeatherData] = useState({});
 	const [location, setLocation] = useState('');
-	console.log(weatherData);
 
 	const searchLocation = (event) => {
 		if (event.key === 'Enter') {
@@ -32,28 +32,38 @@ const App = () => {
 						<p>{weatherData.name}</p>
 					</div>
 					<div className='temp'>
-						{weatherData.main ? <h1>{weatherData.main.temp}°C</h1> : null}
+						{weatherData.main ? (
+							<h1>{weatherData.main.temp.toFixed()}°C</h1>
+						) : null}
 					</div>
 					<div className='description'>
 						{weatherData.weather ? <p>{weatherData.weather[0].main}</p> : null}
 					</div>
 				</div>
-				<div className='bottom'>
-					<div className='feels'>
-						{weatherData.main ? (
-							<p className='bold'>{weatherData.main.feels_like}°C</p>
-						) : null}
-						<p>Feels like</p>
+				{weatherData.name != undefined ? (
+					<div className='bottom'>
+						<div className='feels'>
+							{weatherData.main ? (
+								<p className='bold'>
+									{weatherData.main.feels_like.toFixed()}°C
+								</p>
+							) : null}
+							<p>Feels like</p>
+						</div>
+						<div className='humidity'>
+							{weatherData.main ? (
+								<p className='bold'>{weatherData.main.humidity}%</p>
+							) : null}
+							<p>Humidity</p>
+						</div>
+						<div className='wind'>
+							{weatherData.wind ? (
+								<p className='bold'>{weatherData.wind.speed} m/s</p>
+							) : null}
+							<p>Winds speed</p>
+						</div>
 					</div>
-					<div className='humidity'>
-						<p className='bold'>20%</p>
-						<p>Humidity</p>
-					</div>
-					<div className='wind'>
-						<p className='bold'>12 km/h</p>
-						<p>Winds speed</p>
-					</div>
-				</div>
+				) : null}
 			</div>
 		</div>
 	);
